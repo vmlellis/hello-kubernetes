1291f904cb5d318932e263f0d01c0aaa8a3d5ee0
Victor Lellis <vmlellis@gmail.com>
2016-04-24 00:23:56 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
+
